f1e9ee3e8608b3f4406b4080ddbd8b89c4cf94cc
parvinzaman <parvinzaman.pr@gmail.com>
2023-10-16 17:36:12 +0330

    added repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      weather app
+      <footer>
+        This project was coded by Parvin Zaman and is{" "}
+        <a
+          href="https://github.com/parvinzaman/howday-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced go on github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
